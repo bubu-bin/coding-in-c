1028cd5690aef91ced02879fadf916a68110e2ef
bubu-bin <k.kasp97@gmail.com>
2024-04-05 15:06:55 +0200

    BST Init - Added insert method - Added inOrderTraversal method

---
+++ data-structures/binary-search-tree.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode
+{
+    int value;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} TreeNode;
+
+struct TreeNode *createNode(int value)
+{
+    TreeNode *node = malloc(sizeof(TreeNode));
+
+    node->left = NULL;
+    node->right = NULL;
+    node->value = value;
+
+    return node;
+}
+
+struct TreeNode *insert(struct TreeNode *root, int value)
+{
+    if (root == NULL)
+    {
+        return createNode(value);
+    }
+
+    if (root->value > value)
+    {
+        root->left = insert(root->left, value);
+    }
+    else
+    {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+void inOrderTraversal(struct TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inOrderTraversal(root->left);
+    printf("%d\n", root->value);
+    inOrderTraversal(root->right);
+}
+
+int main(int argc, char const *argv[])
+{
+    struct TreeNode *root = createNode(2);
+    insert(root, 20);
+    insert(root, 30);
+    insert(root, 50);
+    insert(root, 80);
+    insert(root, 32);
+
+    inOrderTraversal(root);
+    return 0;
+}
